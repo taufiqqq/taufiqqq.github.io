f1067c2b27577d80d79998ac0bdaa006f0402688
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2023-02-04 01:11:16 +0800

    code

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("from script file");
+
+let saveFile = () => {
+
+    // Get the data from each element on the form.
+    const name = document.getElementById('txtName');
+    const age = document.getElementById('txtAge');
+    const email = document.getElementById('txtEmail');
+    const country = document.getElementById('selCountry');
+    const msg = document.getElementById('msg');
+
+    // This variable stores all the data.
+    let data =
+        '\r Name: ' + name.value + ' \r\n ' +
+        'Age: ' + age.value + ' \r\n ' +
+        'Email: ' + email.value + ' \r\n ' +
+        'Country: ' + country.value + ' \r\n ' +
+        'Message: ' + msg.value;
+
+    // Convert the text to BLOB.
+    const textToBLOB = new Blob([data], { type: 'text/plain' });
+    const sFileName = 'formData.txt';	   // The file to save the data.
+
+    let newLink = document.createElement("a");
+    newLink.download = sFileName;
+
+    if (window.webkitURL != null) {
+        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    }
+    else {
+        newLink.href = window.URL.createObjectURL(textToBLOB);
+        newLink.style.display = "none";
+        document.body.appendChild(newLink);
+    }
+
+    newLink.click();
+}
