bcf19f2ec804ea59205599f8610ec8423f393c23
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2023-05-25 12:58:51 +0800

    Update

---
+++ UTMWebProg/15-learnPHP/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //single line comment
+    # single line comment
+    /* Multiple
+    Line
+    Comment */
+    /* 
+    echo "Hello World";
+    echo "<br>";
+    echo 'This is a test';
+    echo "<br><p><b>";
+    echo "This is a 2nd test </p></b>"
+     */
+
+    ECHO "HELLO WORLD!<br>";
+    echo "HELLO WORLD!<br>";
+    EcHo "HELLO WORLD!<br>";
+    echo "<brr>";
+
+    #However; all variables are case sensitive
+    /* In the example below, only the first 
+    statement will display the vvalue of the $color
+     variable (this is because $color, $COLOR, and 
+     %coLOR are treated as threee different variables) */
+
+    $color = "red";
+    echo "My car is " . $color . "<br>";
+    echo "My house is " . $COLOR . "<br>";
+    echo "My boat is " . $coLOR . "<br>";
+
+    $TXT = "Hello WOrld!";
+    $x = 5;
+    $y = 10.5;
+    $z = $x + $y;
+    echo $_REQUEST
+?>
+</body>
+</html>
